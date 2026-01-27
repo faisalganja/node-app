b66c8903ac8b50586b673fa0d846c39570322d0c
faisal jamil <faisal.lahoree@gmail.com>
2026-01-27 14:02:18 +0000

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,6 @@
+@Library('jenkins-shared-library@tdk') _
+ecsDeployPipeline(
+    registryName: "my-app",
+    deployToStage: true,
+    containerName: "my-app"
+)
---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello!' });
+});
+
+app.listen(3000, () => console.log('Running'));
+
